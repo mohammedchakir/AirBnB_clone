8336b125b3a1e19ceee07f0fb17b1f3a7329b62c
mohammedchakir <zerroukimedchakir@gmail.com>
2023-12-10 07:05:48 -0800

    correct some faillurs

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -73,7 +73,6 @@ class FileStorage:
             obj_dict = json.load(f)
             obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
-            # TODO: Does this operation involve overwriting or inserting?
             FileStorage.__objects = obj_dict
 
     def attributes(self):
@@ -83,36 +82,36 @@ class FileStorage:
         """
         attributes = {
             "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
+            {"id": str,
+                "created_at": datetime.datetime,
+                "updated_at": datetime.datetime},
             "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
+            {"email": str,
+                "password": str,
+                "first_name": str,
+                "last_name": str},
             "State":
-                     {"name": str},
+            {"name": str},
             "City":
-                     {"state_id": str,
-                      "name": str},
+            {"state_id": str,
+                "name": str},
             "Amenity":
-                     {"name": str},
+            {"name": str},
             "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
+            {"city_id": str,
+                "user_id": str,
+                "name": str,
+                "description": str,
+                "number_rooms": int,
+                "number_bathrooms": int,
+                "max_guest": int,
+                "price_by_night": int,
+                "latitude": float,
+                "longitude": float,
+                "amenity_ids": list},
             "Review":
             {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
+                "user_id": str,
+                "text": str}
+            }
         return attributes
